e12c70acc34cbef7b1a4a5f14fc722bedfd6ee33
Francis <francisbernal14@gmail.com>
2023-03-23 21:44:39 +0100

    feat: add bodyparser package and create post method to handle error if any file was not sent

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 var express = require('express');
 var cors = require('cors');
+var bodyParser = require('body-parser');
 require('dotenv').config()
 
 var app = express();
 
+
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+// My code starts here...
 
+app.post('/api/fileanalyse', (req, res) => {
+  const file = req.body.upfile;
 
+  if (!file) {res.json({error: "You have not sent any file."})}
+  console.log(file);
+})
+
+// My code finishes here.
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
